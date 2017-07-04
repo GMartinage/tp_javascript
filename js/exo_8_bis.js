e4eb514b61e4818fe88065baefc660fb0cdecc4f
Gaelle Martinage <martinage.gaelle@hotmail.fr>
2017-07-04 15:00:01 +0200

    Exercice 8 : changements

--- js/exo_8_bis.js
+++ js/exo_8_bis.js
@@ -5,7 +5,7 @@ var printForm = function() {
         "<p>Prénom : </p>" + document.getElementById("firstname").value +
         "<p>Sexe : </p>" + document.getElementById("gender").value +
         "<p>Âge : </p>" + document.getElementById("age").value +
-        "<p>Adresse : </p>" + document.getElementById("address_1").value + " " + document.getElementById("address_2").value + " " + document.getElementById("address_2").value +
+        "<p>Adresse : </p>" + document.getElementById("address_1").value + " " + document.getElementById("address_3").value + " " + document.getElementById("address_2").value +
         "<p>Pseudo : </p>" + document.getElementById("pseudo").value +
         "<p>Mot de passe : </p>" + document.getElementById("password").value;
 }
