e0ab469b682eaacbc99334948b73ea3c00b9080e
Gaelle Martinage <martinage.gaelle@hotmail.fr>
2017-07-16 19:12:57 +0200

    Création du formulaire, validation si valeurs égales à 100, mise en place du localStorage

--- js/tp_heroes.js
+++ js/tp_heroes.js
@@ -1,15 +1,34 @@
-var agility = document.getElementById('agility').value;
-var strength = document.getElementById('strength').value;
-var endurance = document.getElementById('endurance').value;
+returnNbre = localStorage.getItem("yourHero");
+console.log(localStorage.getItem("yourHero"));
 
-function getRandomNumber() {
-    var numbers = '0123456789';
-    for (var i = 0; i < 2; i++) {
-        numbers[Math.floor(Math.random() * 16)];
+var saveHero = function() {
+
+    var name = document.getElementById('name').value;
+    var agility = parseInt(document.getElementById('agility').value);
+    var strength = parseInt(document.getElementById('strength').value);
+    var endurance = parseInt(document.getElementById('endurance').value);
+    var saved = document.getElementById('save');
+
+    if(agility + strength + endurance === 100) {
+
+        saved.innerHTML = "Your hero is saved";
+        saved.style.color='green';
+        var returnNbre = name + " " + agility + " " + strength + " " + endurance;
+
+        localStorage.setItem("yourHero", returnNbre);
+        console.log(returnNbre);
+
+        newHero = document.getElementById('heroes').appendChild(document.createElement('div'));
+        newHero.id = name;
+        newHero.innerHTML = "Name : " + name + "<br />" +
+            "Agility : " + agility + "<br />" +
+            "Strength : " + strength + "<br />" +
+            "Endurance : " + endurance;
+
     }
-    return numbers;
-}
+    else {
+        saved.innerHTML = "You must have a total of 100.";
+        saved.style.color='red';
+    }
 
-function setRandomNumber(element) {
-    element.value = getRandomNumber();
 }
